eea1688ffcc96b193593e96c253dedd97efe4ac0
Aryanshravan <aryanshravanchouti123@gmail.com>
2026-04-12 18:10:00 +0530

    Made a temperature converter

---
+++ temperatur converter/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to celsius or farehenheit? (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp - 32)* 5 / 9 : (temp * 9 / 5) + 32;
+        System.out.println("the converted temp is:" + newTemp);
+
+
+
+
+        scanner.close();
+
+
+    }
+}
