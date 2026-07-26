7f913d388e79393b139ceffaada4fe60f95d3feb
Aryanshravan <aryanshravanchouti123@gmail.com>
2026-07-26 19:39:39 +0530

    revised today

---
+++ javapractice/main.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String[] args){
+        int n  = 3;
+        for(int i = 0 ; i < n;i++){
+            for(int j = 1;j<=i;i++){
+                System.out.print(i);
+            }
+        System.out.println();
+        }
+  }
+}
